acea7b84e6b6f44d1fe514e6e7f8dee2dd2506f9
Toast <toastgeraet@homeworld.(none)>
2022-04-06 00:06:46 +0200

    create board setup

--- src/Board.vue
+++ src/Board.vue
@@ -10,20 +10,110 @@ function getRandomInt(min: number, max: number): number {
 }
 
 function get_random<T>(array: Array<T>): T {
-  return array[Math.floor((Math.random()*array.length))];
+    return array[Math.floor((Math.random() * array.length))];
 }
 
-const types = ["Lion", "Rhino", "Giraffe", "Gnu", "Elephant", "Mouse"];
+/**
+ * 
+ * z     g
+ * e l r g
+ * 
+ * zebra ur ul dr dl
+ * gnu u d l r
+ * elephant uu dd ll rr
+ * lion u d l r ur ul dr dl
+ * rhino uu dd ll rr ur ul dr dl
+ * giraffe urur ulul drdr dldl
+ * 
+ * gazelle
+ * 
+ */
+
+const types = ["Lion", "Rhino", "Giraffe", "Gnu", "Elephant", "Zebra"];
+
+type CardType = "Lion" | "Rhino" | "Giraffe" | "Gnu" | "Elephant" | "Zebra"
+enum CardTypes {
+    Lion = "Lion",
+    Rhino = "Rhino",
+    Giraffe = "Giraffe",
+    Gnu = "Gnu",
+    Elephant = "Elephant",
+    Zebra = "Zebra",
+}
+
+/**
+ * defined from 0 to 15 
+ * boardCells are numbered left to right, top to bottom
+ */
+const boardSetup: ({ isOpponent: boolean, type: CardType } | null)[][] = [
+    [{
+        isOpponent: true,
+        type: CardTypes.Elephant,
+    },
+    {
+        isOpponent: true,
+        type: CardTypes.Lion,
+    },
+    {
+        isOpponent: true,
+        type: CardTypes.Rhino,
+    },
+    {
+        isOpponent: true,
+        type: CardTypes.Elephant,
+    }],
+    [{
+        isOpponent: true,
+        type: CardTypes.Zebra,
+    },
+        null,
+        null,
+    {
+        isOpponent: true,
+        type: CardTypes.Gnu,
+    }],
+    [{
+        isOpponent: false,
+        type: CardTypes.Zebra,
+    },
+        null,
+        null,
+    {
+        isOpponent: false,
+        type: CardTypes.Gnu,
+    }],
+    [{
+        isOpponent: false,
+        type: CardTypes.Elephant,
+    },
+    {
+        isOpponent: false,
+        type: CardTypes.Lion,
+    },
+    {
+        isOpponent: false,
+        type: CardTypes.Rhino,
+    },
+    {
+        isOpponent: false,
+        type: CardTypes.Giraffe,
+    }],
+]
 
 function getRandomType(): string {
     return get_random(types);
 }
 
+
 </script>
 
 <template>
-    <div v-for="row in 4">
-        <Card v-for="col in 4" :player="getRandomInt(0,2)" :type="getRandomType()"></Card>
+    <div v-for="row in boardSetup">
+        <Card
+            v-for="card in row"
+            :player="card == null ? 0 : (card?.isOpponent ? 2 : 1)"
+            :type="card?.type ?? 'Empty'"
+        ></Card>
     </div>
 </template>
 
