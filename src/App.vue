77cc59de56f9d0c15ffbe7eb4b1b9e57ee8afc52
Toast <toastgeraet@homeworld.(none)>
2022-04-07 22:04:37 +0200

    clean up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 import Board from './Board.vue';
 </script>
 
@@ -16,12 +15,8 @@ import Board from './Board.vue';
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 60px;
-  margin-left: 60px;
-}
-
-.logo {
-  height: 100px;
+  margin-top: 30px;
+  margin-left: 30px;
 }
 
 .j-left {
