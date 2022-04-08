4b4c1c758b4df2d5d25eebca11e713865eb488e9
Toast <toastgeraet@homeworld.(none)>
2022-04-08 20:54:25 +0200

    add ability for cards to move to valid targets

--- src/Board.vue
+++ src/Board.vue
@@ -3,9 +3,27 @@ import { reactive, ref } from '@vue/reactivity';
 import Card from './components/Card.vue';
 import { Add, CardDef, CardType, CardTypes, Pos } from './helpers/helpers';
 
-function onTapCard(boardPos: [number, number], card: CardDef) {
+function onTapCard(boardPos: Pos) {
     return function (direction: any, event: any) {
+        const card = boardState[boardPos[1]][boardPos[0]];
+        console.log(boardPos, card);
         
+        const target = validMoveTargets.find(p => [...p].every((num, idx) => boardPos[idx] == num));
+        if (target) {
+            const sourceIdx = selectedCard[0] + selectedCard[1] * 4;
+            const targetIdx = boardPos[0] + boardPos[1] * 4;
+            const flatBoard = boardState.flat();
+            let temp = flatBoard[targetIdx];
+            flatBoard[targetIdx] = flatBoard[sourceIdx];
+            flatBoard[sourceIdx] = temp;
+                        
+            boardState.splice(0, boardState.length, ...chunkArray(flatBoard, 4));
+
+            selectedCard.splice(0, 2, -1, -1);
+            validMoveTargets.splice(0, validMoveTargets.length);
+            return;
+        }
+
         if ([...selectedCard].every((num, idx) => boardPos[idx] == num)) {
             selectedCard.splice(0, 2, -1, -1);
             validMoveTargets.splice(0, validMoveTargets.length);
@@ -15,11 +33,26 @@ function onTapCard(boardPos: [number, number], card: CardDef) {
         if (currentPlayer.value == getCardOwner(card)) {
             selectedCard.splice(0, 2, ...boardPos);
             validMoveTargets.splice(0, validMoveTargets.length, ...getCardMoves(card, boardPos))
-            console.log(getCardMoves(card, boardPos))
-            console.log(validMoveTargets);
+            // console.log(getCardMoves(card, boardPos))
+            // console.log(validMoveTargets);
+            return;
         }
 
     };
+}
+
+function chunkArray<T>(array: T[], chunkSize: number): T[][] {
+    return array.reduce((resultArray: T[][], item: T, index) => {
+        const chunkIndex = Math.floor(index / chunkSize)
+
+        if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+
+        resultArray[chunkIndex].push(item)
+
+        return resultArray;
+    }, [])
 }
 
 const currentPlayer = ref(1);
@@ -43,7 +76,7 @@ function getCardMoves(card: CardDef, boardPos: Pos): Pos[] {
     return card.getMoves().map(moveVector => Add(boardPos, moveVector)).filter(p => p.every(i => i >= 0 && i < 4));
 }
 
-const boardState: (CardDef)[][] = [
+const boardState: (CardDef)[][] = reactive([
     [new CardDef(CardTypes.Elephant, true),
     new CardDef(CardTypes.Lion, true),
     new CardDef(CardTypes.Rhino, true),
@@ -60,7 +93,13 @@ const boardState: (CardDef)[][] = [
     new CardDef(CardTypes.Lion, false),
     new CardDef(CardTypes.Rhino, false),
     new CardDef(CardTypes.Giraffe, false)],
-]
+]);
+
+// setInterval(()=>{
+//     let tmp = boardState[3][0];
+//     boardState[3][0] = boardState[3][3]
+//     boardState[3][3] = tmp
+// }, 3000)
 
 
 </script>
@@ -70,11 +109,11 @@ const boardState: (CardDef)[][] = [
         <Card
             v-for="(card, c) in row"
             :player="getCardOwner(card)"
-            :type="card?.type ?? 'Empty'"
+            :type="card.type"
             :selected="isSelected([c, r])"
             :target="isValidMoveTarget([c, r])"
-            :pos="[c, r]"
-            v-touch:tap="onTapCard([c, r], card)"
+            :pos="[c, r]"            
+            v-touch:tap="onTapCard([c, r])"
         ></Card>
     </div>
 </template>
